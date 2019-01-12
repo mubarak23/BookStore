41ec8385db3bccb123f25ef56d14f966984b37af
Mubarak Aminu <mubarakaminu340@gmail.com>
2019-01-12 18:09:31 +0100

    add a model let us add book to the store

--- resources/views/main_home.blade.php
+++ resources/views/main_home.blade.php
@@ -10,8 +10,58 @@
       <h1 class="jumbotron-heading">Album example</h1>
       <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
       <p>
-        <a href="#" class="btn btn-primary my-2">Main call to action</a>
+        <a href="#" class="btn btn-primary my-2" >Main call to action</a>
         <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+          Add Book
+        </button>
+        <!-- //modal for showing add book form -->
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+              <form>
+              <div class="form-row">
+                <div class="col col-md-6">
+                  <legend class="col-form-label col-sm-2 pt-0">Book Title</legend>
+                  <input type="text" name="title" class="form-control" placeholder="First name">
+                </div>
+                <div class="col-md-6">
+                  <legend class="col-form-label col-sm-2 pt-0">Book Category</legend>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                    <label class="form-check-label" for="gridRadios1">
+                      First radio
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                    <label class="form-check-label" for="gridRadios2">
+                      Second radio
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                  <legend class="col-form-label col-sm-2 pt-0">Book Description</legend>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </p>
     </div>
   </section>
